3e63d3367d41a26132b08ed02f8dd38ccf2968bb
pvdoanh <pvdoanh.ce@gmail.com>
2020-08-09 09:37:44 +0700

    add code for final project

---
+++ final_project/student.py
@@ -0,0 +1,18 @@
+
+'''
+Đề bài chương trình quản lý sinh viên
+Khai báo kiểu dữ liệu SinhVien có các trường họ tên, giới tính, tuổi, điểm toán – lý – hóa và điểm trung bình.
+Nhập vào danh sách N sinh viên
+Xuất danh sách N sinh viên
+Tính điểm trung bình cho N sinh viên
+Sắp xếp N sinh viên theo thứ tự tăng dần của điểm trung bình
+Xếp loại N sinh viên
+Xuất danh sách N sinh viên ra file
+Viết chương trình dạng menu cho phép sử dụng các tính năng trên
+'''
+students = []
+id = ''
+name = ''
+email = ''
+medium_score = 0
+
