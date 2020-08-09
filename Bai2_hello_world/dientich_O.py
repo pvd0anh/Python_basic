ade60cba8bcc6e013e36e8f8f1caaa70c4bc533e
pvdoanh <pvdoanh.ce@gmail.com>
2020-08-09 09:33:56 +0700

    add code for lesson 2 - Hello World!

---
+++ Bai2_hello_world/dientich_O.py
@@ -0,0 +1,8 @@
+def main(r):
+    pi = 3.14
+
+    dien_tich = pi*r*r
+
+    print('dien tich hinh tron co ban kinh {} la {:f}'.format(r, dien_tich))
+if __name__ == "__main__":
+    main(r=4)
---
+++ Bai2_hello_world/hello_world.py
@@ -0,0 +1,32 @@
+# Hiển thị "Hello World!"
+print("Hello World!")
+
+x= 4
+y= 5
+print(x + y)
+
+x = 4
+y = 5
+print(('x > y is',x>y))
+
+num1 = 4
+num2 = 5
+
+print(("Line 1 - Value of num1 : ", num1))
+print(("Line 2 - Value of num2 : ", num2))
+
+num1 = 4
+num2 = 5
+
+res = num1 + num2
+res += num1
+
+print(("Line 1 - Result of + is ", res))
+
+# Bước 1: Gán giá trị cho num1 và num2
+
+# Bước 2: Cộng giá trị của num1 và num2 (4 + 5 = 9)
+
+# Bước 3: Cộng num1 vào đầu ra của Bước 2 (9 + 4)
+
+# Bước 4: Chương trình sẽ in kết quả cuối cùng là 13
